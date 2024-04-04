6f9ae0e05577e069ad52cd3b69b20e2d8731be0d
AndresCabralTB <andres.cabralrs@udlap.mx>
2024-04-04 17:05:04 -0600

    Initial Commit

---
+++ BotonDeAyudaIOS/BotonDeAyudaIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotonDeAyudaIOSApp.swift
+//  BotonDeAyudaIOS
+//
+//  Created by Andres Cabral on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BotonDeAyudaIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BotonDeAyudaIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BotonDeAyudaIOS
+//
+//  Created by Andres Cabral on 04/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
